8ef5c30415ce278af6400f6b5880da91e3a4b841
Luis Felipe Narvaez Gomez <ing.felipenarvaez017@gmail.com>
2024-03-14 21:36:19 -0500

    parada de emergencia

--- src/material_oxidation_machine/my_test/tkinter/tkinter_test_7/test/read_MCU.py
+++ src/material_oxidation_machine/my_test/tkinter/tkinter_test_7/test/read_MCU.py
@@ -1,12 +1,14 @@
 #pip install pyserial
 import serial
 
-import proube as pr
+import init_text as pr
+from Code.code_gpio import output_7_seven as seven
+import emergency_stop as warning
 
 #Comando Linux para determinar el nombre de dispositivo serial:
 # dmesg | grep -v disconnect | grep -Eo "tty(ACM|USB)." | tail -1
 #aparecera algo como ttyACM0
-
+#aparecera ttyUSB0
 
 while True:
     ser = serial.Serial('/dev/ttyUSB0', 9600) #tasa de transferencia de 9600 baudios
@@ -26,7 +28,8 @@ while True:
         print("sensor 3: "+sensor_3)
         print("sensor 4: "+sensor_4)
         
-        pr.init_register(sensor_1, sensor_2, sensor_3, sensor_4)        
+        pr.init_register(sensor_1, sensor_2, sensor_3, sensor_4)
+        warning.STOP(sensor_4)        
 
     except KeyboardInterrupt:
         break
